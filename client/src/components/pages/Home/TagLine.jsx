56ac33347804e848a32f083d2b8f08d8df97e4d4
Akhil Patel <akhilsf@gmail.com>
2021-12-14 15:12:32 -0800

    styling change to landing page

--- client/src/components/pages/Home/TagLine.jsx
+++ client/src/components/pages/Home/TagLine.jsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  justify-content: space-around;
+  padding: 25px;
 `;
 
 const TagLineContainer = styled.div`
@@ -14,10 +16,11 @@ const TagLineContainer = styled.div`
 
   const Pic = styled.img`
   object-fit: cover;
-  `;
+`;
 
   const Text = styled.p`
   display: flex;
+  width: 50%;
   align-items: center;
   color: #f5f3f2;
   font-size: clamp(20px, 4vw, 40px);
@@ -30,10 +33,11 @@ const TagLine = ({ screenWidth }) => {
   return (
     <Container style={{
       flexDirection: screenWidth < widthThreshold ? 'column' : undefined,
+      height: screenWidth < widthThreshold ? '45vh' : undefined,
     }}
     >
       <TagLineContainer style={{
-        width: screenWidth < widthThreshold ? '100%' : '50%',
+        width: screenWidth < widthThreshold ? '100%' : '48%',
       }}
       >
         <Text style={{
@@ -52,7 +56,7 @@ const TagLine = ({ screenWidth }) => {
       <Pic
         src="assets/homePhoto.jpeg"
         style={{
-          width: screenWidth < widthThreshold ? undefined : '50%',
+          width: screenWidth < widthThreshold ? undefined : '48%',
           height: screenWidth < widthThreshold ? '20vh' : '40vh',
         }}
       />
