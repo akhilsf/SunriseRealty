f5c1616b3bf32ea98c913cd159136dadd4aea945
Akhil Patel <akhilsf@gmail.com>
2022-01-27 09:10:57 -0800

    new bio for anish

--- client/src/components/pages/Team/BioFeed.jsx
+++ client/src/components/pages/Team/BioFeed.jsx
@@ -27,7 +27,7 @@ const BioFeed = ({ screenWidth }) => {
       shortID: 'anish',
       name: 'Anish Patel',
       title: 'Managing Partner, Sunrise Realty Investments',
-      bio: 'Anish specializes in detailed market analysis, financial modeling, asset management, and investor relations. His expertise stems from his 15+ year support of his family hotel business, where he supports marketing, corporate partnerships, and interior and exterior design activities; his 7+ year career within the biotechnology industry, where he informs major investments through market analysis, financial modeling, and strategic planning; and his 5+ year, personal experience owning and operating rental properties. Anish values continued education and growth, and is currently refining his skillsets and knowledge through real estate specific courses at the Berkeley Haas School of Business and through membership of several mastermind and investor groups.',
+      bio: 'Anish specializes in detailed market analysis, financial modeling, asset management, and investor relations. His expertise stems from his 15+ year support of his family hotel business, where he supports marketing and interior and exterior design activities; his 7+ year career within the biotechnology industry, where he guides company strategy through strategic partnerships, market analysis, financial modeling, and long-range planning; and his 5+ year, personal experience owning and operating rental properties. He is also a member of several mastermind and investor groups and the co-host of the Bay Area Multifamily Masters Meetup.',
     },
     {
       shortID: 'akash',
